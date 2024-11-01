603db0189c6de75aa6d9f76a0638de9840cc435b
Romuald-VT <romualdtchatcho@gmail.com>
2024-11-02 00:45:23 +0100

    configuration de production

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
+const {logger} = require('./logging/logger')
 
 
 const dbConnection = async()=>{
 
     try{
         await mongoose.connect(process.env.DB_URL)
-        console.log("connected to the database !")
+        logger.info('connecte a la base de donnees')
     }
     catch(error)
     {
-        console.log('error: '+error.message)
+        logger.error(error.message)
+
     }
 }
 
--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const express = require('express')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const { userRouter } = require('./route/userRoute')
 const { customerRouter } = require('./route/customerRoute')
+const compression = require('compression')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit');
 
 const app = express()
 const port = process.env.SERVER_PORT || 4000
 
 
 dbConnection()
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
+app.use(compression())
+app.use(helmet())
+app.use(limiter);
 
 
 app.use("/api/users",userRouter)
