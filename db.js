20bd1b671668816fae6b75d3e506c1ef4333ce1d
Romuald-VT <romualdtchatcho@gmail.com>
2024-11-02 01:27:01 +0100

    new commit trois

--- db.js
+++ db.js
@@ -5,12 +5,7 @@ const {logger} = require('./logging/logger')
 const dbConnection = async()=>{
 
     try{
-        await mongoose.connect(process.env.DB_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            ssl: true,
-            sslValidate: true,
-        })
+        await mongoose.connect(process.env.DB_URL)
         logger.info('connecte a la base de donnees')
     }
     catch(error)
