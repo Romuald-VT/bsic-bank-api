e0b3799d16322cea341939afaaddf58d659eb79d
Romuald-VT <romualdtchatcho@gmail.com>
2024-11-02 01:08:23 +0100

    new commit

--- index.js
+++ index.js
@@ -6,21 +6,18 @@ const { userRouter } = require('./route/userRoute')
 const { customerRouter } = require('./route/customerRoute')
 const compression = require('compression')
 const helmet = require('helmet')
-const rateLimit = require('express-rate-limit');
 
 const app = express()
 const port = process.env.SERVER_PORT || 4000
 
 
 dbConnection()
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 app.use(compression())
 app.use(helmet())
-app.use(limiter);
 
 
 app.use("/api/users",userRouter)
